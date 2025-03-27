475b6899ab3c1d7011f4a7c4b64c85411037a2c0
Umang455 <umangsomani7@gmail.com>
2025-03-27 17:34:43 +0530

    Updated cors

--- app.js
+++ app.js
@@ -9,36 +9,16 @@ require("./db");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
-var courseRouter = require('./routes/courses')
-var studentRouter = require("./routes/students")
-var activityRouter = require("./routes/activities")
-var eventRouter = require("./routes/events")
+var courseRouter = require("./routes/courses");
+var studentRouter = require("./routes/students");
+var activityRouter = require("./routes/activities");
+var eventRouter = require("./routes/events");
 
 var app = express();
 const serveIndex = require("serve-index");
 
-
-let allowedOrigins = [
-  "http://localhost:3000",
-  "https://eventify-frontend-rho.vercel.app/"
-];
-
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      // allow requests with no origin
-      // (like mobile apps or curl requests)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        let msg =
-          "The CORS policy for this site does not " +
-          "allow access from the specified Origin.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
+// Enable CORS for all origins
+app.use(cors());
 
 app.use(
   "/public",
@@ -49,12 +29,9 @@ app.use(
   })
 );
 
-
-
 app.use("/public", express.static("photos"));
 
-
-// view engine setup
+// View engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
@@ -66,23 +43,22 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-app.use("/courses", courseRouter)
-app.use("/students", studentRouter)
-app.use('/activities', activityRouter)
-app.use('/events', eventRouter)
+app.use("/courses", courseRouter);
+app.use("/students", studentRouter);
+app.use("/activities", activityRouter);
+app.use("/events", eventRouter);
 
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
+  // Render the error page
   res.status(err.status || 500);
   res.render("error");
 });
